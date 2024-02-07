51fc652d3a06ccd9f9653596fff5b9c99ca53041
Manikanta Muppidi <128898564+Mani-M-1@users.noreply.github.com>
2024-02-07 22:45:01 +0530

    first commit

---
+++ src/components/LikedProducts/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+
+const LikedProducts = () => {
+    return (
+        <div className='liked-products-page__bg-container'>
+            Your wishlist is empty.
+        </div>
+    )
+}
+
+
+export default LikedProducts;
